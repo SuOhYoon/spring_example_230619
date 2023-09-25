33132b28be2c409256e7258a469e1a84a331d67d
학원 <test@test.com>
2023-09-25 21:46:33 +0900

    예제 진행중

--- src/main/java/com/example/lesson03/Lesson03Ex01RestController.java
+++ src/main/java/com/example/lesson03/Lesson03Ex01RestController.java
@@ -1,14 +1,24 @@
 package com.example.lesson03;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.lesson03.bo.ReviewBO;
+import com.example.lesson03.domain.Review;
 
 @RestController
 public class Lesson03Ex01RestController {
-
+	@Autowired
+	private ReviewBO reviewBO;
 	// 요청 url : http://localhost/lesson03/ex01
 	@RequestMapping("/lesson03/ex01")
-	public Review ex01() {
-		return ;
+	public Review ex01(
+			 @RequestParam(value="id") int id) {// 필수 파라미터
+			// @RequestParam(value="id", required=true)int id) // 필수 파라미터
+			// @RequestParam(value="id", required=false) Integer id) // 비필수 파라미터
+			// @RequestParam(value="id", defaultValue="1")int id) { // 비필수 파라미터, 디폴트값 1
+		return reviewBO.selectReview(id);
 	}
 }
--- src/main/java/com/example/lesson03/bo/ReviewBO.java
+++ src/main/java/com/example/lesson03/bo/ReviewBO.java
@@ -1,15 +1,29 @@
 package com.example.lesson03.bo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.lesson03.domain.Review;
+import com.example.lesson03.mapper.ReviewMapper;
 
 @Service
 public class ReviewBO {
-
+	@Autowired	
+	private ReviewMapper reviewMapper;
 	// input
 	// output: Review(단건)
-	public Review getReview() {
-		return
+	public Review selectReview(int id) {
+		return reviewMapper.selectReview(id);
+	}
+	
+	// input: Review(단건)
+	// output: int(성공된 행의 개수)
+	public int addReview(Review review) {
+		return reviewMapper.insertReview(review);
+	}
+	
+	// addReviewAsField(4, "콤비네이션R", "김바다", 5.0, "역시 맛있다!!!");
+	public int addReviewAsField(int storeId, String menu, String userName, Double point, String review) {
+		return reviewMapper.insertReviewAsField(storeId, menu, userName, point, review);
 	}
 }
